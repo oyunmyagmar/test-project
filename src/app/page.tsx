d9a965c0867f2e56d0816906386252a693617d55
25lp5026 <25lp5026@G301-CC0193.local>
2025-11-19 12:27:32 +0800

    q

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import { Sidebar } from "@/components/ui/sidebar";
 import Header from "./_components/Header";
 
 const HomePage = () => {
   return (
     <div className="bg-linear-to-r from-[#323a5c] via-[#003153] to-[#86608e] w-full h-screen">
       {/* <img src={"/pinebaatar-bg.svg"} /> */}
-      <Sidebar />
       <Header />
     </div>
   );
